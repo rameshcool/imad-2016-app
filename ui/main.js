20afcca169b5dc9982fd13f68b2c0ad563628736
rameshcool <rameshcool@users.noreply.github.com>
2016-10-25 20:36:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ element.innerHTML = "My Page";
 
 //move the image
 var img = document.getElementById("madi");
-img.onClick = function() {
+img.onclick = function() {
     img.style.marginLeft = "100px";
 };
