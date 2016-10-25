3dfdef006043e5e77878ae58c2e71f0d5eea6503
rameshcool <rameshcool@users.noreply.github.com>
2016-10-25 20:43:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = "My Page";
 
 //move the image
 var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
 img.onclick = function() {
-    img.style.marginLeft = "100px";
+    var interval = setInterval(moveRight, 100);
+    
 };
