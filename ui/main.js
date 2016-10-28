59778889ddf2819eb685c43f3e0f9baeaf54203a
rameshcool <rameshcool@users.noreply.github.com>
2016-10-28 20:10:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ button.onclick = function() {
           if(request.status === 200) {
               var counter = request.responeText;
               var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
+              span.innerHTML = count.toString();
           }
       }
       //Not done yet
