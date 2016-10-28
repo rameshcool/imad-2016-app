c2e6fc728c24b845ca4f8bfc7044b302799f6dd5
rameshcool <rameshcool@users.noreply.github.com>
2016-10-28 20:19:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ img.onclick = function() {
 
 //Counter Code
 var button = document.getElementById('counter');
-
+var counter = 0;
 button.onclick = function() {
     
     //Create a request object
