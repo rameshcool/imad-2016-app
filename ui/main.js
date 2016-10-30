57d93c70c0de232ffe8d60be0841e725b694a98e
rameshcool <rameshcool@users.noreply.github.com>
2016-10-30 21:25:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,8 +43,7 @@ button.onclick = function() {
 
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
    
@@ -73,6 +72,8 @@ submit.onclick = function() {
     };
     
     //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://rameshcool.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 
