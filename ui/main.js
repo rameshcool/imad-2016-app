005ec60591b571f531b9793c091883f6fcd5af17
rameshcool <rameshcool@users.noreply.github.com>
2016-10-30 20:53:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,15 +48,6 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
    
-    //Create a request object
-    
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          //Take some action
-          if(request.status === 200) {
          //Create a request object
     
     var request = new XMLHttpRequest();
