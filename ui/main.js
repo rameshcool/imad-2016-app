af082ca2b302fad48d0dcb3a6979d716bf43dfdf
rameshcool <rameshcool@users.noreply.github.com>
2016-10-30 20:46:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,27 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 50);
     
 };*/
+//Counter Code
+var button = document.getElementById('counter');
 
+button.onclick = function() {
+ //Create a request object
+    
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
+    var counter = request.responseText;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+          }
+      }
+      //Not done yet
+    };
+    
 //Make the request
     request.open('GET', 'http://rameshcool.imad.hasura-app.io/counter', true);
     request.send(null);
@@ -37,15 +57,33 @@ submit.onclick = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
           //Take some action
           if(request.status === 200) {
+         //Create a request object
+    
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
              //Capture a list of names and render it as a list
     var names = request.responseText;
     names = JSON.parse(names);
     var list = '';
-    for(i=0;i<names.length;i++) {
+    for(var i=0;i<names.length;i++) {
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+          }
+      }
+      //Not done yet
+    };
+    
+    //Make the request
+    request.open('GET', 'http://rameshcool.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
           }
       }
       //Not done yet
