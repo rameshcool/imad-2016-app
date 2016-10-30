b5ba06875b7ec6d19b2dacc2c0093ff78f0ceae9
rameshcool <rameshcool@users.noreply.github.com>
2016-10-30 13:29:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,11 +16,18 @@ img.onclick = function() {
     
 };*/
 
-//Counter Code
-var button = document.getElementById('counter');
+//Make the request
+    request.open('GET', 'http://rameshcool.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
 
-button.onclick = function() {
-    
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+   
     //Create a request object
     
     var request = new XMLHttpRequest();
@@ -30,31 +37,25 @@ button.onclick = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
           //Take some action
           if(request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      }
-      //Not done yet
-    };
-    
-    //Make the request
-    request.open('GET', 'http://rameshcool.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-//Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    //Make a request to the server and send the name
-    //Capture a list of names and render it as a list
-    var names = ['Windows', 'Mac OSX', 'Linux', 'Android'];
+             //Capture a list of names and render it as a list
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for(i=0;i<names.length;i++) {
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+          }
+      }
+      //Not done yet
+    };
+    
+    //Make the request
+    request.open('GET', 'http://rameshcool.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
+
+
+    
 };
