cc16d221c7fda7ec8e2d98de951175f4eb14689c
rameshcool <rameshcool@users.noreply.github.com>
2016-11-20 01:15:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,8 +133,8 @@ app.get('/articles/:articleName', function(req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     
-    //SELECT * FROM articles WHERE title = '" ; DELETE WHERE a = 'asdf'
-     pool.query("SELECT * FROM articles WHERE title = '" + req.params.articleName + "'", function(err, result) {
+    //SELECT * FROM articles WHERE title = '\" ; DELETE WHERE a = \'asdf'
+     pool.query("SELECT * FROM articles WHERE title = $1" , [req.params.articleName] , function(err, result) {
        if (err) {
            res.status(500).send(err.toString());
        }  else {
