cca39cca0c35f983dc5ec57db0c9314e65c3a044
rameshcool <rameshcool@users.noreply.github.com>
2016-11-19 18:05:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,12 +129,23 @@ app.get('/submit-name',function(req, res) {//URL: /submit-name?name=xxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res) {
+app.get('/articles/:articleName', function(req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
+    
+     pool.query("SELECT * FROM articles WHERE title = " + req.params.articleName, function(err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       }  else {
+           if (result.rows.length === 0) {
+               res.status(404).send('Article not found');
+           } else {
+               var articleData = result.rows[0];       
+               res.send(createTemplate(articleData));
+           }
+       }
+     });       
+   });
 
 app.get('/article-two', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
