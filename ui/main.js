7f3dba122ca76b47a684f025ee22aabf609dec59
rameshcool <rameshcool@users.noreply.github.com>
2016-11-27 01:32:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-/*console.log('Loaded!');
+console.log('Loaded!');
 
 //change the text of the main-text div
 var element = document.getElementById("main-text");
@@ -14,7 +14,7 @@ function moveRight () {
 img.onclick = function() {
     var interval = setInterval(moveRight, 50);
     
-};*/
+};
 //Counter Code
 var button = document.getElementById('counter');
 
@@ -42,7 +42,7 @@ button.onclick = function() {
 };
 
 
-//Submit name
+//Submit username/password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -57,25 +57,25 @@ submit.onclick = function() {
           //Take some action
           if(request.status === 200) {
              //Capture a list of names and render it as a list
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list = '';
-    for(var i=0;i<names.length;i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-     
+   console.log('user logged in');
+   alert('Logged in successfully');
+          } else if (request.status === 403) {
+              alert('Username/password is incorrect');
+          } else if (request.status === 500) {
+              alert('Something went wrong on the server');
           }
       }
       //Not done yet
     };
     
     //Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://rameshcool.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+   console.log(username);
+   console.log(password);
+    request.open('POST', 'http://rameshcool.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 
 };
 
