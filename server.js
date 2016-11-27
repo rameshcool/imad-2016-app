58fd6d58ef49bc2a92898519283f5a1c14fb9b0f
rameshcool <rameshcool@users.noreply.github.com>
2016-11-27 20:54:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'rameshcool',
@@ -15,7 +16,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret : 'someRandomSecretValue',
+    cookie : { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -103,18 +107,30 @@ app.get('/hash/:input', function(req, res) {
              var salt = dbString.split('$')[2];
              var hashedPassword = hash(password, salt); //Creating a hash based on the password submitted and the original salt
         if (hashedPassword === dbString) {
-         res.send('credentials correct!');
-         
-         // Set the session
-         
-         
-         } else {
+            
+            // Set the session 
+            req.session.auth = {userId: result.rows[0].id};
+            // set cookie with a session id
+            // internally, on the server side, it maps the session id to an object
+            // { auth: {userId}}
+            
+               res.send('credentials correct!');                  
+        
+                 } else {
              res.send(403).send('username/password is invalid');
          }
          }
-        
-     }    
+            }    
     });
+        });
+        
+        app.get('/check-login', function(req, res) {
+            if (req.session && req.session.auth && req.session.auth.userId) {
+                res.send('You are logged in:' + req.session.auth.userId.toString());
+                
+                } else {
+                    res.send('You are not logged in');
+                }
         });
 
 var pool = new Pool(config);
