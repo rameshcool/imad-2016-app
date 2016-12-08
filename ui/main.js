0f34e10f3c7f02219048d905da31ac15edd47588
rameshcool <rameshcool@users.noreply.github.com>
2016-12-08 09:02:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -80,26 +80,18 @@ submit.onclick = function() {
       }
       // Not done yet
     };
-    console.log('user logged in');
-    alert('Logged in successfully');
-          } else if (request.status === 403) {
-              alert('Username/password is incorrect');
-          } else if (request.status === 500) {
-              alert('Something went wrong on the server');
-          }
-      }
-      //Not done yet
-    };
+    
     
     //Make the request
      var username = document.getElementById('username').value;
      var password = document.getElementById('password').value;
      console.log(username);
      console.log(password);
-     request.open('POST', 'http://rameshcool.imad.hasura-app.io/login', true);
+     request.open('POST', '/login', true);
      request.setRequestHeader('Content-Type', 'application/json');
      request.send(JSON.stringify({username: username, password: password}));
-
+     submit.value = 'Logging in....';
 };
+
 
     
