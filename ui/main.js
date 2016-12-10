50a7af5077103cafee74023cdccb7cb69ddd199b
rameshcool <rameshcool@users.noreply.github.com>
2016-12-11 01:35:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,57 +1,15 @@
-console.log('Loaded!');
 function loadLoginForm () {
-    var loginHtml ='
-    <h3>Login/Register to unlock awesome features<h3/>
-    <input type="text' id="username" placeholder="username" />
+    
+    var loginHtml = 
+    '<h3>Login/Register to unlock awesome features</h3>
+    <input type="text" id="username" placeholder="username" />
     <input type="password" id="password" />
     <br/><br/>
     <input type="submit" id="login_btn" value="Login" />
-    <input type="submit" id="register_btn" value="Register" />
-   ';
+    <input type="submit" id="register_btn" value="Register" />';
+   
    document.getElementById('login_area').innerHTML = loginHtml;
-}
-
-/*//change the text of the main-text div
-var element = document.getElementById("main-text");
-element.innerHTML = "My Page";
-
-//move the image
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
-    
-};*/
-//Counter Code
-/*var button = document.getElementById('counter');
-
-button.onclick = function() {
- //Create a request object
-    
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          //Take some action
-          if(request.status === 200) {
-    var counter = request.responseText;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-          }
-      }
-      //Not done yet
-    };
-    
-//Make the request
-    request.open('GET', 'http://rameshcool.imad.hasura-app.io/counter', true);
-    request.send(null);
-};*/
-
+   
 //Submit username/password to login
 
 var submit = document.getElementById('submit_btn');
