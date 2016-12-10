f8567767608af93d35a3956e177971cfd7f51bed
rameshcool <rameshcool@users.noreply.github.com>
2016-12-10 20:43:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -158,6 +158,8 @@ register.onclick = function () {
       var articleData = JSON.parse(this.responseText);
       for (var i=0; i<articleData.length; i++) {
           content += '<li>
-          
+          <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+          (${articleData[i].date.split('T')[0]})</li>';
       }
+      
  }
