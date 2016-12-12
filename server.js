c892ec9d9604a3a681b16dec5f6416cf2ab89ea3
rameshcool <rameshcool@users.noreply.github.com>
2016-12-12 19:53:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -243,6 +243,15 @@ app.get('/articles/:articleName', function(req, res) {
        }
      });       
    });
+   
+   app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 
 app.get('/ui/: filename', function (req, res) {
