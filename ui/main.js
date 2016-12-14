f45ca4552bab25eb7d3371cd2026e98d33ae40d2
rameshcool <rameshcool@users.noreply.github.com>
2016-12-14 18:17:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 function loadLoginForm () {
-         var loginHtml = ' 
-    <h3>Login/Register to unlock awesome features!</h3>
-    <input type="text" id="username" placeholder="username" />
-    <input type="password" id="password" />
+         var loginHtml = '
+    '<h3>Login/Register to unlock awesome features!</h3>';
+    '<input type="text" id="username" placeholder="username" />';
+    '<input type="password" id="password" />';
     <br/><br/>
-    <input type="submit" id="login_btn" value="Login" />
-    <input type="submit" id="register_btn" value="Register" />
+    '<input type="submit" id="login_btn" value="Login" />';
+    '<input type="submit" id="register_btn" value="Register" />;
     ';
-    
+
     document.getElementById('login_area').innerHTML = loginHtml;
    
 //Submit username/password to login
@@ -84,8 +84,8 @@ register.onclick = function () {
  
  function loadLoggedInUser (username) {
      var loginArea = document.getElementById('login_area');
-     loginArea.innerHTML = '<h3> Hi <i>${username}</i></h3>
-                            <a href = "/logout">Logout</a>';
+     loginArea.innerHTML = '<h3> Hi <i>${username}</i></h3>';
+                            '<a href = "/logout">Logout</a>';
  }
  
  function loadLogin () {
@@ -115,9 +115,9 @@ register.onclick = function () {
       var content = '<ul>';
       var articleData = JSON.parse(this.responseText);
       for (var i=0; i<articleData.length; i++) {
-          content += '<li>
-          <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-          (${articleData[i].date.split('T')[0]})</li>';
+          content += '<li>';
+          '<a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>';
+           '(${articleData[i].date.split('T')[0]})</li>';
       }
       content += "</ul>";
       articles.innerHTML = content;
