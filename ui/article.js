c31f739d29561dfeae8909c91df056b33129f82c
rameshcool <rameshcool@users.noreply.github.com>
2017-01-01 01:01:16 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,7 +3,6 @@
 var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
-    
     var commentFormHtml = `
     <h5>Submit a comment</h5>
     <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
